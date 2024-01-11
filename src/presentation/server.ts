d072d83afd13fae9f9ef42233b3160ec4c899b66
lumusitech <lumusitech@gmail.com>
2024-01-11 19:53:58 -0300

    feat: use check service use case into server

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,20 +1,12 @@
+import { CheckService } from '../domain/use-cases/checks/check-service'
 import { CronService } from '../presentation/cron/cron-service'
 
 export class Server {
   public static start() {
     console.log('🚀 Server started...')
 
-    CronService.createJob('*/5 * * * * *', () => {
-      const date = new Date()
-      console.log('fired each 5 sec', date)
-    })
-    CronService.createJob('*/2 * * * * *', () => {
-      const date = new Date()
-      console.log('fired each 2 sec', date)
-    })
-    CronService.createJob('*/3 * * * * *', () => {
-      const date = new Date()
-      console.log('fired each 3 sec', date)
+    CronService.createJob('*/10 * * * * *', () => {
+      new CheckService().execute('https://www.google.com')
     })
   }
 }
