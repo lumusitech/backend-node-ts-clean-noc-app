b1b1e88bf2eb3b2f86658ff52b5536c60da49376
lumusitech <lumusitech@gmail.com>
2024-01-12 19:32:34 -0300

    refactor: add new properties to logEntity

--- src/domain/entities/log.entity.ts
+++ src/domain/entities/log.entity.ts
@@ -4,26 +4,33 @@ export enum LogSeveretyLevel {
   high = 'high',
 }
 
+export interface LogEntityOptions {
+  message: string
+  level: LogSeveretyLevel
+  origin: string
+  createdAt?: Date
+}
+
 export class LogEntity {
   public level: LogSeveretyLevel
   public message: string
-  public createdAt: Date
+  public origin: string
+  public createdAt?: Date
 
-  constructor(message: string, level: LogSeveretyLevel) {
+  constructor(options: LogEntityOptions) {
+    const { message, level, origin, createdAt = new Date() } = options
+
     this.message = message
     this.level = level
-    this.createdAt = new Date()
+    this.origin = origin
+    this.createdAt = createdAt
   }
 
   static fromJson = (json: string): LogEntity => {
     const { message, level, createdAt } = JSON.parse(json)
 
-    // if (!message) throw new Error(`${message} is required`)
-    // if (!level) throw new Error(`${level} is required`)
-    // if (!createdAt) throw new Error(`${createdAt} is required`)
+    const log = new LogEntity({ level, message, origin, createdAt })
 
-    const log = new LogEntity(level, message)
-    log.createdAt = new Date(createdAt)
     return log
   }
 }
