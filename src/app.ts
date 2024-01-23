73eda98144c94ec2ab5136fe87e190ce97ba9934
Luciano Figueroa <lumusitech@gmail.com>
2024-01-23 12:58:44 -0300

    feat: add direct create a read into app.ts - bad practice

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import { envs } from './config/plugins/envs.plugin'
-import { MongoDatabase } from './data/mongo-db'
+import { LogModel, MongoDatabase } from './data/mongo-db'
 import { Server } from './presentation/server'
 ;(async () => {
   main()
@@ -10,6 +10,20 @@ async function main() {
     mongoUrl: envs.MONGO_URL,
     dbName: envs.MONGO_DB_NAME,
   })
+
+  // create a document that we want to insert
+  // const newLog = await LogModel.create({
+  //   message: 'Test message from mongo',
+  //   origin: 'App.ts',
+  //   level: 'low',
+  // })
+
+  // await newLog.save()
+
+  // Once created, we can query it
+  const logs = await LogModel.find({ message: { $regex: /mongo 0/i } })
+
+  console.log({ logs })
 
   Server.start()
 }
