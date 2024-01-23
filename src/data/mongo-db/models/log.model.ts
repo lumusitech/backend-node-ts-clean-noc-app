6cda01fd19e409a10d3e0c9df742f3c94d9432a0
Luciano Figueroa <lumusitech@gmail.com>
2024-01-23 11:48:42 -0300

    feat: add log mongoose model

---
+++ src/data/mongo-db/models/log.model.ts
@@ -0,0 +1,22 @@
+import { Schema, model } from 'mongoose'
+
+const logSchema = new Schema({
+  message: {
+    type: String,
+    required: true,
+  },
+  level: {
+    type: String,
+    enum: ['low', 'medium', 'high'],
+    default: 'low',
+  },
+  origin: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+})
+
+export const LogModel = model('Log', logSchema)
